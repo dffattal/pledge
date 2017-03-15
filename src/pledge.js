33d90b3b79cde2ce792013e27acbd2d813351cde
Daniel Fattal <dffattal@gmail.com>
2017-03-15 17:14:08 -0400

    chapter 3 completed

--- src/pledge.js
+++ src/pledge.js
@@ -7,25 +7,63 @@ Promises Workshop: build the pledge.js ES6-style promise library
 var $Promise = function(executor) {
   this._state = 'pending'
   this._value = undefined
+  this._handlerGroups = []
   if (typeof executor === 'function') {
     executor(this._internalResolve.bind(this), this._internalReject.bind(this))
   }
 }
 
 $Promise.prototype._internalResolve = function(data) {
-    if (this._state === 'pending') {
-      this._state = 'fulfilled'
-      this._value = data
-    }
+  if (this._state === 'pending') {
+    this._state = 'fulfilled'
+    this._value = data
+    this._callHandlers(this._value)
   }
+}
+
 $Promise.prototype._internalReject = function(err) {
-    if (this._state === 'pending') {
-      this._state = 'rejected'
-      this._value = err
+  if (this._state === 'pending') {
+    this._state = 'rejected'
+    this._value = err
+    this._callHandlers(this._value)
+  }
+}
+
+$Promise.prototype.then = function(successHandler, errorHandler) {
+  if(this._state === 'pending') {
+    if (typeof successHandler !== 'function') {
+      successHandler = null
+    }
+    if (typeof errorHandler !== 'function') {
+      errorHandler = null
+    }
+    this._handlerGroups.push({successCb: successHandler, errorCb: errorHandler})
+  } else if (this._state === 'fulfilled') {
+    successHandler(this._value)
+  } else {
+    if (errorHandler) {
+      errorHandler(this._value)
     }
   }
+}
 
+$Promise.prototype._callHandlers = function(value) {
+  console.log(this)
+  if (this._state === 'fulfilled') {
+    this._handlerGroups.forEach(function(cbObj) {
+      cbObj.successCb(value)
+    })
+  } else {
+    this._handlerGroups.forEach(function(cbObj) {
+      cbObj.errorCb(value)
+    })
+  }
+  this._handlerGroups = []
+}
 
+$Promise.prototype.catch = function(errorHandler) {
+  this.then(null, errorHandler)
+}
 
 
 /*-------------------------------------------------------
